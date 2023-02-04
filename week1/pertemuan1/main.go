99e5c0cff546f46c50799c4c7d0adb3997ba2ca6
unknown <mknetprojects@gmail.com>
2023-02-04 13:54:20 +0700

    add pertemuan1

---
+++ week1/pertemuan1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// hello go
+// func main()  {
+// 	fmt.Println("Hello world")
+// }
+
+
+// variable
+func main()  {
+	var firstName string = "john"
+	var lastName string
+	lastName = "wick"
+
+	fmt.Printf("halo %s %s!\n", firstName, lastName)
+
+// 	var message = `Nama saya "John Wick".
+// Salam kenal.
+// Mari belajar "Golang".`
+
+// 	fmt.Println(message)
+
+	// var value = (((2+6)%3)*4 - 2) / 3
+	// var isEqual = (value == 2)
+
+	// fmt.Printf("nilai %d (%t) \n", value, isEqual)
+}
